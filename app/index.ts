f2ef891ef7672a2e99a536035040e66614972d83
Pedro Gabriel Lima <pedrogglima@gmail.com>
2019-04-27 10:06:04 -0300

    created view list-users

--- app/index.ts
+++ app/index.ts
@@ -2,30 +2,67 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import * as templates from './templates.ts';
 
-document.body.innerHTML = templates.main();
+const listUsers = users => {
+  // load users
+  const mainElement = document.body.querySelector('.app-main');
+  mainElement.innerHTML = templates.listUsers({users});
 
-const mainElement = document.body.querySelector('.app-main');
-const alertsElement = document.body.querySelector('.app-alerts');
+  // add delete event on buttons
+  const deleteButtons = mainElement.querySelectorAll('button.delete');
+  for (let i = 0; i < deleteButtons.length; i++) {
+    const deleteButton = deleteButtons[i];
+    deleteButton.addEventListener('click', event => {
+      deleteUser(deleteButton.getAttribute('data-user-id'));
+    });
+  }
+}
 
-mainElement.innerHTML = templates.welcome();
-alertsElement.innerHTML = templates.alert({
-  type: 'success',
-  message: 'Setup inicial funcionando!',
-});
+// delete user by id
+const deleteUser = async (userId) => {
+  try {
+    console.log('usuario foi deletado!');
+  } catch (err) {
+    showAlert(err);
+  }
+}
 
-/**
-* Use Window location hash to show the specified view.
-*/
+// show alert message
+const showAlert = (message, type = 'danger') => {
+  const alertsElement = document.body.querySelector('.app-alerts');
+  const html = templates.alert({type, message});
+  alertsElement.insertAdjacentHTML('beforeend', html);
+};
+
+
+//Use Window location hash to show the specified view.
 const showView = async () => {
+  const mainElement = document.body.querySelector('.app-main');
   const [view, ...params] = window.location.hash.split('/');
+
   switch (view) {
     case '#welcome':
       mainElement.innerHTML = templates.welcome();
+      break;
+    case '#users':
+      try {
+        listUsers([
+          { "id": "1", "first_name": "morpheus", "last_name": "zion" },
+          { "id": "2", "first_name": "matheus", "last_name": "jardim" },
+          { "id": "3", "first_name": "lucas", "last_name": "montreal" }
+        ])
+      } catch (err) {
+        showAlert(err);
+        window.location.hash = '#welcome';
+      }
       break;
     default:
       throw Error(`Error 404: ${view}`);
   }
 };
 
-window.addEventListener('hashchange', showView);
-showView().catch(err => window.location.hash = '#welcome');
+// Page setup.
+(async () => {
+  document.body.innerHTML = templates.main();
+  window.addEventListener('hashchange', showView);
+  showView().catch(err => window.location.hash = '#welcome');
+})();
--- app/templates.ts
+++ app/templates.ts
@@ -47,6 +47,42 @@ export const welcome = Handlebars.compile(`
   </div>
 `);
 
+export const listUsers = Handlebars.compile(`
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-sm-1 col-md-1 col-lg-1"></div>
+      <div class="col-sm-9 col-md-9 col-lg-9">
+        <h1>Lista de Usuários</h1>
+        <div class="container">
+          {{#if users.length}}
+            {{#each users}}
+              <hr />
+              <div class="row">
+                <ul class="user-info">
+                  <li>{{first_name}}</li>
+                  <li>{{last_name}}</li>
+                  <li><button class="btn btn-link delete" data-user-id="{{id}}">
+                        delete
+                      </button>
+                  </li>
+                </ul>
+              </div>
+
+            {{/each}}
+          {{else}}
+            <div class="row">
+              <hr />
+              <h4>Não há usuários cadastrados.</h4>
+            </div>
+          {{/if}}
+        </div>
+      </div>
+      <div class="col-sm-2 col-md-2 col-lg-2"></div>
+    </div>
+  </div>
+`);
+
+
 export const alert = Handlebars.compile(`
   <div class="alert alert-{{type}} alert-dismissible" role="alert">
     <button class="close" data-dismiss="alert" aria-label="Close">
